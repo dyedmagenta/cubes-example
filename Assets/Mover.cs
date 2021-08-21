8cd84777467490565d92cbe59450a1e398a075e6
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2021-08-21 17:29:49 +0200

    Add project files

---
+++ Assets/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 1f;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        direction = Random.onUnitSphere;
+    }
+
+    private void Update()
+    {
+        var position = transform.position;
+        position += direction * (speed * Time.deltaTime);
+        transform.position = position;
+    }
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    public float speed = 1f;
+    
+    void Update()
+    {
+        var rotation = transform.rotation.eulerAngles;
+        var newY = rotation.y + speed * Time.deltaTime;
+        rotation.y = newY;
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
+
+
+
+
